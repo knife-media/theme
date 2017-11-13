8987e59447daa83ae69b03d15df7ae76ae437e1a
Anton Lukin <anton@lukin.me>
2017-11-13 23:56:02 +0300

    add spacing stripe

--- app/front-page.php
+++ app/front-page.php
@@ -21,9 +21,9 @@ get_header();
 	$news = get_category_by_slug('news');
 
 	$sidebar_related = new WP_Query([
-		'category__not_in' => [$news->term_id],
+		'category__in' => 14,
 		'post__not_in' => [$post->ID],
-		'posts_per_page' => 8,
+		'posts_per_page' => 4,
 		'offset' => 0,
 		'ignore_sticky_posts' => 1,
 		'post_status' => 'publish'
@@ -31,14 +31,41 @@ get_header();
 
 	if($sidebar_related->have_posts()) :
 ?>
-
-  <section class="stripe stripe--mindmap">
+   <section class="stripe stripe--triple">
 
 <?php
 		while($sidebar_related->have_posts()) :
 			$sidebar_related->the_post();
 
-			get_template_part('template-parts/stripe', 'mindmap');
+			get_template_part('template-parts/stripe', 'cover');
+		endwhile;
+
+		wp_reset_postdata();
+?>
+
+	</section>
+
+  <section class="stripe stripe--spacing">
+
+<?php
+		while($sidebar_related->have_posts()) :
+			$sidebar_related->the_post();
+
+			get_template_part('template-parts/stripe', 'spacing');
+		endwhile;
+
+		wp_reset_postdata();
+?>
+
+	</section>
+
+   <section class="stripe stripe--single">
+
+<?php
+		while($sidebar_related->have_posts()) :
+			$sidebar_related->the_post();
+
+			get_template_part('template-parts/stripe', 'cover');
 		endwhile;
 
 		wp_reset_postdata();
--- app/template-parts/stripe-spacing.php
+++ app/template-parts/stripe-spacing.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Stripe template with image
+ * Spacing is a transparent stripe with optional sticker
  *
  * Stripe is a single content item using in any loop.
  * Can be 1/3, 1/2 or full screen width
@@ -11,24 +11,22 @@
 $category = get_the_category(); ?>
 
 <article class="stripe__item">
-	<?php if(isset($category[0])) : ?>
+ 	<?php if(isset($category[0])) : ?>
 	<a class="stripe__item-head" href="<?php echo get_category_link($category[0]->term_id); ?>"><?php echo $category[0]->cat_name; ?></a>
 	<?php endif; ?>
 
 	<a class="stripe__item-link" href="<?php the_permalink(); ?>">
 
-		<div class="stripe__item-image">
-			<?php the_post_thumbnail('', ['class' => 'stripe__item-thumbnail']); ?>
-		</div>
+		<img class="stripe__item-sticker" src="<?php echo get_post_meta($post->ID, 'post_icon', true); ?>"/>
 
 		<footer class="stripe__item-footer">
-			<p class="stripe__item-title"><?php the_title(); ?></p>
-
 			<div class="stripe__item-info">
 				<span class="stripe__item-meta"><?php knife_theme_authors() ?></span>
 
 				<time class="stripe__item-meta" datetime="<?php the_time('c'); ?>"><?php the_time('d F');?></time>
 			</div>
+
+			<p class="stripe__item-title"><?php the_title(); ?></p>
 		</footer>
 
 	</a>
