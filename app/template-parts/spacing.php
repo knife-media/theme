05ca0beb3a1888be1bb5c3aad516ceb5edfd3f19
Anton Lukin <anton@lukin.me>
2017-11-17 13:50:24 +0300

    add spacing template

---
+++ app/template-parts/spacing.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Spacing is a transparent stripe with optional sticker
+ *
+ * Stripe is a single content item using in any loop.
+ * Can be 1/3, 1/2 or full screen width
+ *
+ * @package knife-theme
+ * @since 1.1
+ */
+$category = get_the_category();
+?>
+
+
