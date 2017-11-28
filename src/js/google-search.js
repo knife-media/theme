aac32c8cff1033efa509a90037a17156a73cbd12
Anton Lukin <anton@lukin.me>
2017-11-28 19:45:00 +0300

    google search release

--- src/js/google-search.js
+++ src/js/google-search.js
@@ -1,10 +1,69 @@
-(function(){
-	return;
+(function() {
+	var parent = '.search';
+	var holder = 'search-gcse';
+	var detect = 'gsc-results';
 
- window.__gcse = {
-		parsetags: 'explicit',
-		callback: gcseCallback
-	};
+	if(document.querySelector(parent).length < 1)
+		return false;
+
+	var view = null;
+
+	var push = function() {
+		if(document.readyState !== 'complete')
+    		return google.setOnLoadCallback(push, true);
+
+		// Render results to holder element
+		google.search.cse.element.render({
+			gname: holder,
+			div: holder,
+			tag: 'searchresults-only',
+			attributes: {
+				enableHistory: false,
+				gaQueryParameter: 's',
+				queryParameterName: 's'
+			}
+		});
+
+		// Update fake results on search input
+		document.getElementById('search-input').oninput = function() {
+			var element = google.search.cse.element.getElement(holder);
+
+			element.execute(this.value);
+		}
+
+		// Create observer to detect new results in gsce fake block
+		var observer = window.MutationObserver || window.WebKitMutationObserver;
+
+		view = new observer(function(mutations) {
+			for (var i = 0; i < mutations.length; ++i) {
+				if (!mutations[i].target.classList.contains(detect))
+					continue;
+
+				return make();
+			}
+		});
+
+		view.observe(document.getElementById(holder), {subtree: true, attributes: false, childList: true, characterData: false});
+	}
+
+
+    var init = function() {
+		// Create fake div for google results
+		var fake = document.createElement("div");
+
+		fake.id = holder;
+		fake.style.display = 'none';
+
+		// Append fake div to base selector
+		document.querySelector(parent).appendChild(fake);
+
+		// Prepare gcse callback
+		window.__gcse = {
+			parsetags: 'explicit',
+			callback: push
+		};
+
+		// Load gsce to page
 		var cx = '009571386059139339715:mqsuxgtrwyo';
 		var gcse = document.createElement('script');
 		gcse.type = 'text/javascript';
@@ -12,61 +71,50 @@
 		gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
 		var s = document.getElementsByTagName('script')[0];
 		s.parentNode.insertBefore(gcse, s);
-
-
-function gcseCallback() {
-	var result = document.createElement("div");
-
-google.search.cse.element.render(
-        {
-gname:'gsearch',
-          div: "results",
-          tag: 'searchresults-only',
-attributes: {
-enableHistory: false,
-gaQueryParameter: 's',
-queryParameterName: 's'
-}
-         });
-
-};
-
-	var updateResults = function() {
-		var res = document.getElementById('results');
-		var items = res.querySelectorAll('.gs-result');
-
-		document.getElementById('xx').innerHTML = '';
-
-		for(var i = 0; i < items.length; i++) {
-   			var it = document.createElement('div');
-			it.innerHTML = items[i].querySelector('a.gs-title').textContent;
-
-			document.getElementById('xx').appendChild(it);
-		}
-
 	}
 
-	var input = document.getElementById('search-input');
 
-	input.oninput = function() {
-		var element = google.search.cse.element.getElement('gsearch');
-		element.execute(input.value);
-	}
+	var make = function() {
+		var result = document.getElementById('search-results');
+		var source = document.getElementById(holder).querySelectorAll('.gs-result');
 
-	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-	new MutationObserver(function(mutations) {
-		for (var i = 0; i < mutations.length; ++i) {
-			if (mutations[i].target.classList.contains('gsc-results')) {
-				updateResults();
-				break;
+		var html = '';
+
+		if(document.getElementById('search-input').value.length > 0) {
+
+			for(var i = 0; i < source.length; i++) {
+				if(!source[i].querySelector('a.gs-title') || !source[i].querySelector('.gs-snippet'))
+					return false;
+
+				// TODO: Rework this behaviour
+				html += '<div class="search__results-item"><a class="search__results-link" href="' + source[i].querySelector('a.gs-title').href + '">';
+				html += '<p class="search__results-head">' + source[i].querySelector('a.gs-title').textContent + '</p>';
+				html += '<p class="search__results-text">' + source[i].querySelector('.gs-snippet').textContent + '</p>';
+				html += '</a></div>';
 			}
 		}
-	}).observe(document.getElementById('results'), {
-		subtree:       true,
-		attributes:    false,
-		childList:     true,
-		characterData: false
+
+		// Clear nodes. Faster than innerHTML
+		while (result.firstChild) {
+			result.removeChild(result.firstChild);
+		}
+
+		result.innerHTML = html;
+	}
+
+
+
+	// Init google cse on search layer open
+	document.querySelector('.topline .toggle--search').addEventListener('click', function(e) {
+		e.preventDefault();
+
+		document.getElementById('search-input').focus();
+
+		if(this.classList.contains('toggle--expand'))
+			return false;
+
+		if(typeof window.__gcse === 'undefined')
+			return init();
 	});
 
 })();
-
