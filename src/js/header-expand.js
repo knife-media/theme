4dcc619be1dabf2dff616cfa9a9e5c50e1ba67b0
Anton Lukin <anton@lukin.me>
2017-12-14 18:56:57 +0300

    merge search-expand to header-expand

--- src/js/header-expand.js
+++ src/js/header-expand.js
@@ -1,17 +1,46 @@
 (function() {
-	var parent = '.topline__menu';
+    var menu = document.getElementById('toggle-menu');
+	var search = document.getElementById('toggle-search');
 
-	if(document.querySelector(parent) === null)
-		return false;
 
-	document.querySelector('.topline .toggle--menu').addEventListener('click', function(e) {
+	// Toggle menu bar
+	menu.addEventListener('click', function(e) {
 		e.preventDefault();
 
-		document.querySelector(parent).classList.toggle('topline__menu--expand');
-
 		if(document.querySelector('.search').classList.contains('search--expand'))
-			document.querySelector('.topline .toggle--search').click();
+			search.click();
+
+		document.querySelector('.topline__menu').classList.toggle('topline__menu--expand');
 
 		return this.classList.toggle('toggle--expand');
 	});
+
+
+	// Toggle search bar
+	search.addEventListener('click', function(e) {
+		e.preventDefault();
+
+ 		if(document.querySelector('.topline__menu').classList.contains('topline__menu--expand'))
+			menu.click();
+
+		document.querySelector('.search').classList.toggle('search--expand');
+		document.body.classList.toggle('body--search');
+
+		return this.classList.toggle('toggle--expand');
+	});
+
+
+	// Close menu and search on ESC
+	window.addEventListener('keydown', function(e) {
+		e = e || window.event;
+
+		if(e.keyCode !== 27)
+			return false;
+
+		if(document.querySelector('.search').classList.contains('search--expand'))
+			search.click();
+
+ 		if(document.querySelector('.topline__menu').classList.contains('topline__menu--expand'))
+			menu.click();
+	}, true);
 })();
