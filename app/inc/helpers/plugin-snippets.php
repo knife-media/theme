41909a8d8a5820997a88e71bec18e2c33a6b6a05
Anton Lukin <anton@lukin.me>
2018-01-16 22:22:08 +0300

    add social image plugin snippet

--- app/inc/helpers/plugin-snippets.php
+++ app/inc/helpers/plugin-snippets.php
@@ -20,3 +20,35 @@ add_filter('coauthors_posts_link', function($args) {
 
 	return $args;
 });
+
+
+/**
+ * Add custom text and font for social image generator
+ *
+ * @link https://github.com/antonlukin/social-image/blob/master/src/classes/Generation.php
+ */
+add_filter('social_image_texts', function($texts, $params) {
+	$fonts = get_template_directory() . '/assets/fonts/';
+
+	$texts = [
+		[
+			"text" => 'KNIFE.MEDIA',
+			"posx" => 65,
+			"posy" => 60,
+			"file" => $fonts . 'formular/formular-black.ttf',
+			"size" => 22,
+			"color" => '#ffe634'
+		],
+
+		[
+			"text" => wordwrap($params['text'], 1024 / 20),
+			"posx" => 65,
+			"posy" => 150,
+			"file" => $fonts . 'formular/formular-medium.ttf',
+			"size" => 46,
+			"color" => '#ffe634'
+		]
+	];
+
+	return $texts;
+}, 10, 2);
