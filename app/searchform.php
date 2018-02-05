cd02da1a30254cf62be5dc3d14b1c8939c99e8d0
Anton Lukin <anton@lukin.me>
2018-02-05 22:37:38 +0300

    remove safari search autocorrect

--- app/searchform.php
+++ app/searchform.php
@@ -13,7 +13,7 @@
 	<div class="search__layer block">
 
 		<div class="search__field">
-			<input class="search__field-input" type="text" id="search-input" placeholder="Поиск&hellip;" autofocus>
+			<input class="search__field-input" type="text" id="search-input" placeholder="Поиск&hellip;" autocorrect="off" autocomplete="off" spellcheck="false">
 		</div>
 
 		<div class="search__results" id="search-results"></div>
