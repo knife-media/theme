57334b16de28ff3c05513bd07764d40b3b9d59c6
Anton Lukin <anton@lukin.me>
2018-03-13 23:43:10 +0300

    fix zen feed content-type

--- app/core/include/feeds/zen.php
+++ app/core/include/feeds/zen.php
@@ -2,10 +2,11 @@
 /**
  * Yandex.Zen feed
  */
-header('Content-Type: ' . feed_content_type('rss2') . '; charset=' . get_option('blog_charset'), true);
+header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 
 echo '<?xml version="1.0" encoding="' . get_option( 'blog_charset' ) . '"?' . '>';
 ?>
+
 <rss version="2.0"
      xmlns:content="http://purl.org/rss/1.0/modules/content/"
      xmlns:dc="http://purl.org/dc/elements/1.1/"
