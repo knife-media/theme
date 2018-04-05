364df2c922be4018f12ee5befa4a7a9bd9790853
Anton Lukin <anton@lukin.me>
2018-04-05 14:26:08 +0300

    move push-template from footer.php to module

--- app/footer.php
+++ app/footer.php
@@ -63,17 +63,6 @@
 
 </footer>
 
-<div class="push push--hide">
-	<p class="push__promo"><?php _e('Получать последние обновления&nbsp;сайта', 'knife-theme'); ?></p>
-
-	<button class="push__button" data-action="Push Notifications — Accept">
-		<span class="icon icon--notify"></span>
-		<?php _e('Подписаться', 'knife-theme'); ?>
-	</button>
-
-	<button class="push__close" data-action="Push Notifications — Decline"></button>
-</div>
-
 <?php wp_footer(); ?>
 </body>
 </html>
