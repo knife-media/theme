ec0f44a8916b283eb6bce549ea2dea40d56c1a82
Anton Lukin <anton@lukin.me>
2018-05-24 17:16:48 +0300

    remove span tags from tinymce / #42

--- app/core/modules/mce-buttons.php
+++ app/core/modules/mce-buttons.php
@@ -10,52 +10,65 @@
 
 
 if (!defined('WPINC')) {
-	die;
+    die;
 }
 
 
 new Knife_MCE_Buttons;
 
 class Knife_MCE_Buttons {
-	public function __construct() {
-		// button tag
-		add_action('admin_init', [$this, 'init_mce']);
-	}
+    public function __construct() {
+        // button tag
+        add_action('admin_init', [$this, 'init_mce']);
+
+        // configure global tinymce
+        add_filter('tiny_mce_before_init', [$this, 'configure_tinymce']);
+    }
 
 
-	/**
-	 * Init tinymce plugins
-	 */
-	public function init_mce() {
-		if(!current_user_can('edit_posts') && !current_user_can('edit_pages'))
-			return;
+    /**
+     * Init tinymce plugins
+     */
+    public function init_mce() {
+        if(!current_user_can('edit_posts') && !current_user_can('edit_pages'))
+            return;
 
-		if(get_user_option('rich_editing') !== 'true')
-			return;
+        if(get_user_option('rich_editing') !== 'true')
+            return;
 
-		add_filter('mce_buttons', [$this, 'register_buttons']);
-		add_filter('mce_external_plugins', [$this, 'add_plugins']);
-	}
-
-
-	/**
-	 * Add js plugins file
-	 */
-	public function add_plugins($plugins) {
- 		$include = get_template_directory_uri() . '/core/include';
-
-		$plugins['push'] = $include . '/scripts/mce-buttons-push.js';
-
-		return $plugins;
-	}
+        add_filter('mce_buttons', [$this, 'register_buttons']);
+        add_filter('mce_external_plugins', [$this, 'add_plugins']);
+    }
 
 
-	/**
-	 * Register buttons in editor panel
-	 */
-	public function register_buttons($buttons) {
-		array_push($buttons, 'push');
+    /**
+     * Add js plugins file
+     */
+    public function add_plugins($plugins) {
+        $include = get_template_directory_uri() . '/core/include';
 
-		return $buttons;
-	}
+        $plugins['push'] = $include . '/scripts/mce-buttons-push.js';
+
+        return $plugins;
+    }
+
+
+    /**
+     * Register buttons in editor panel
+     */
+    public function register_buttons($buttons) {
+        array_push($buttons, 'push');
+
+        return $buttons;
+    }
+
+
+    /**
+     * Remove annoying span tags after google docs pasting
+     */
+    public function configure_tinymce($settings) {
+        $settings['extended_valid_elements']="span[!class]";
+
+        return $settings;
+    }
 }
