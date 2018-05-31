e8f7a265ce6a594f2ade31fc1a6b135ec0953afa
Anton Lukin <anton@lukin.me>
2018-06-01 01:29:04 +0300

    add custom banner style for feature widget

--- app/core/widgets/feature.php
+++ app/core/widgets/feature.php
@@ -10,98 +10,133 @@
 
 
 class Knife_Feature_Widget extends WP_Widget {
-  public function __construct() {
-    $widget_ops = [
-      'classname' => 'feature',
-      'description' => __('Выводит фичер на всю ширину со стикером', 'knife-theme'),
-      'customize_selective_refresh' => true
-    ];
+    public function __construct() {
+        $widget_ops = [
+            'classname' => 'feature',
+            'description' => __('Выводит фичер на всю ширину со стикером', 'knife-theme'),
+            'customize_selective_refresh' => true
+        ];
 
-    parent::__construct('knife_theme_feature', __('[НОЖ] Фичер', 'knife-theme'), $widget_ops);
-  }
+        parent::__construct('knife_theme_feature', __('[НОЖ] Фичер', 'knife-theme'), $widget_ops);
+    }
 
 
-  /**
-   * Outputs the content of the widget
-   */
-  public function widget($args, $instance) {
-		$defaults = ['title' => '', 'item' => '', 'link' => ''];
-		$instance = wp_parse_args((array) $instance, $defaults);
+    /**
+     * Outputs the content of the widget
+     */
+    public function widget($args, $instance) {
+        $defaults = ['title' => '', 'link' => '', 'sticker' => ''];
 
-		extract($instance);
+        $instance = wp_parse_args((array) $instance, $defaults);
 
-        $post = url_to_postid($link);
+        extract($instance);
 
-		// Don't show empty link
-		if(empty($item) || empty($link))
-			return;
+        // Don't show empty link
+        if(empty($title) || empty($link))
+            return;
+
+        $post_id = url_to_postid($link);
 
         // Don't show self link inside single post
-		if(is_single() && get_queried_object_id() === $post)
-			return;
+        if(is_single() && get_queried_object_id() === $post_id)
+            return;
 
-		echo $args['before_widget'];
 
-		set_query_var('widget_item', $item);
-		set_query_var('widget_link', $link);
+        echo $args['before_widget'];
 
-        if($post > 0) {
-          set_query_var('widget_post', $post);
-          set_query_var('widget_slug', get_post_field('post_name', $post));
+        // right icon
+        $widget_right = '<span class="icon icon--right"></span>';
+
+        // set additional widget class to link
+        $widget_class = 'widget__link';
+
+        // set default widget image
+        $widget_image = '';
+
+        if($post_id > 0) {
+            $widget_class =  $widget_class . ' widget__link--banner';
         }
 
-		get_template_part('template-parts/widgets/feature');
+        // get sticker from widget options or post meta
+        if(strlen($sticker) < 1) {
+            $sticker = get_post_meta($post_id, '_knife-sticker', true);
+        }
 
-		echo $args['after_widget'];
+        // append widget_sticker if image exists
+
+        if(!empty($sticker)) {
+            $widget_image = sprintf('<img class="widget__sticker" src="%1$s" alt="%2$s">',
+                esc_url($sticker), esc_attr($title)
+            );
+        }
+
+        // set widget title
+        $widget_title = sprintf('<p class="widget__title">%s</p>',
+            sanitize_text_field($title)
+        );
+
+        // set widget item
+        $widget_item = sprintf('<div class="widget__item block">%s</div>',
+            $widget_title . $widget_image . $widget_right
+        );
+
+        // and finally print widget link
+        printf('<a class="%2$s" href="%1$s">%3$s</a>', esc_url($link),
+            $widget_class, $widget_item
+        );
+
+        echo $args['after_widget'];
   }
 
 
-  /**
-   * Outputs the options form on admin
-   */
-  function form($instance) {
-		$defaults = ['title' => '', 'item' => '', 'link' => ''];
-		$instance = wp_parse_args((array) $instance, $defaults);
+    /**
+     * Outputs the options form on admin
+     */
+    function form($instance) {
+        $defaults = ['title' => '', 'link' => '', 'sticker' => ''];
 
-		printf(
-			'<p><label for="%1$s">%3$s</label><input class="widefat" id="%1$s" name="%2$s" type="text" value="%4$s"><small>%5$s</small></p>',
-			esc_attr($this->get_field_id('title')),
-			esc_attr($this->get_field_name('title')),
-			__('Название виджета:', 'knife-theme'),
-			esc_attr($instance['title']),
- 			__('Не будет отображаться на странице', 'knife-theme')
-		);
+        $instance = wp_parse_args((array) $instance, $defaults);
 
-		printf(
-			'<p><label for="%1$s">%3$s</label><input class="widefat" id="%1$s" name="%2$s" type="text" value="%4$s"></p>',
-			esc_attr($this->get_field_id('item')),
-			esc_attr($this->get_field_name('item')),
-			__('Заголовок статьи', 'knife-theme'),
-			esc_attr($instance['item'])
-		);
+        printf(
+            '<p><label for="%1$s">%3$s</label><input class="widefat" id="%1$s" name="%2$s" type="text" value="%4$s"><small>%5$s</small></p>',
+            esc_attr($this->get_field_id('title')),
+            esc_attr($this->get_field_name('title')),
+            __('Заголовок фичера', 'knife-theme'),
+            esc_attr($instance['title']),
+             __('Отобразится на странице', 'knife-theme')
+        );
 
- 		printf(
-			'<p><label for="%1$s">%3$s</label><input class="widefat" id="%1$s" name="%2$s" type="text" value="%4$s"></p>',
-			esc_attr($this->get_field_id('link')),
-			esc_attr($this->get_field_name('link')),
-			__('Ссылка с фичера', 'knife-theme'),
-			esc_attr($instance['link'])
-		);
-	}
+        printf(
+            '<p><label for="%1$s">%3$s</label><input class="widefat" id="%1$s" name="%2$s" type="text" value="%4$s"></p>',
+            esc_attr($this->get_field_id('link')),
+            esc_attr($this->get_field_name('link')),
+            __('Ссылка с фичера', 'knife-theme'),
+            esc_attr($instance['link'])
+        );
+
+        printf(
+            '<p><label for="%1$s">%3$s</label><input class="widefat" id="%1$s" name="%2$s" type="text" value="%4$s"><small>%5$s</small></p>',
+            esc_attr($this->get_field_id('sticker')),
+            esc_attr($this->get_field_name('sticker')),
+            __('Ссылка на стикер', 'knife-theme'),
+            esc_attr($instance['sticker']),
+            __('По умолчанию отобразится стикер записи', 'knife-theme')
+        );
+    }
 
 
-  /**
-   * Processing widget options on save
-   */
-  public function update($new_instance, $old_instance) {
-		$instance = $old_instance;
+    /**
+     * Processing widget options on save
+     */
+    public function update($new_instance, $old_instance) {
+        $instance = $old_instance;
 
-		$instance['title'] = sanitize_text_field($new_instance['title']);
- 		$instance['item'] = sanitize_text_field($new_instance['item']);
-  	$instance['link'] = esc_url($new_instance['link']);
+        $instance['title'] = sanitize_text_field($new_instance['title']);
+        $instance['link'] = esc_url($new_instance['link']);
+        $instance['sticker'] = esc_url($new_instance['sticker']);
 
-    return $instance;
-  }
+        return $instance;
+    }
 }
 
 
@@ -109,5 +144,5 @@ class Knife_Feature_Widget extends WP_Widget {
  * It is time to register widget
  */
 add_action('widgets_init', function() {
-	register_widget('Knife_Feature_Widget');
+    register_widget('Knife_Feature_Widget');
 });
