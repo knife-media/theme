40aafdf582aa5db56bbba02e3f0d8a4b80733b52
Anton Lukin <anton@lukin.me>
2018-10-08 15:38:24 +0300

    disable aggresive wordpress update checks

--- app/functions.php
+++ app/functions.php
@@ -168,6 +168,20 @@ add_action('init', function() {
 });
 
 
+// Disable Aggressive Updates
+// @link https://github.com/mihdan/mihdan-disable-aggressive-updates
+if(is_admin()) {
+    remove_action('admin_init', '_maybe_update_core');
+    remove_action('admin_init', '_maybe_update_plugins');
+    remove_action('admin_init', '_maybe_update_themes');
+
+    remove_action('load-plugins.php', 'wp_update_plugins');
+    remove_action('load-themes.php', 'wp_update_themes');
+
+    add_filter('pre_site_transient_browser_' . md5($_SERVER['HTTP_USER_AGENT']), '__return_true');
+}
+
+
 // Disable embeds
 add_action('wp_enqueue_scripts', function() {
     wp_deregister_script('wp-embed');
