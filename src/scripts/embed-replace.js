8e687c8e59184bd14c07d66ee25ad81aff1a7f68
Anton Lukin <anton@lukin.me>
2018-10-19 22:45:57 +0300

    add embed-replace script

---
+++ src/scripts/embed-replace.js
@@ -0,0 +1,24 @@
+/**
+ * Replace embed links with iframes
+ *
+ * @since 1.5
+ */
+
+(function() {
+  var youtube = document.querySelectorAll( ".embed-youtube" );
+
+  for (var i = 0; i < youtube.length; i++) {
+      youtube[i].addEventListener( "click", function(e) {
+        e.preventDefault();
+
+        var iframe = document.createElement( "iframe" );
+
+        iframe.setAttribute( "frameborder", "0" );
+        iframe.setAttribute( "allowfullscreen", "" );
+        iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+        this.appendChild(iframe)
+    });
+  };
+
+})();
