e9ecd54d140d6a62f528c95c147eb557a91d273e
Anton Lukin <anton@lukin.me>
2019-04-29 15:47:25 +0300

    add sent condition to distribute metabox

--- app/core/include/templates/distribute-metabox.php
+++ app/core/include/templates/distribute-metabox.php
@@ -24,143 +24,147 @@
 
         <?php foreach($items as $uniqid => $item) : ?>
             <div class="item">
-                <?php if(isset($item['results'])) : ?>
-<?php
-                    $item = wp_parse_args($item, [
-                        'results' => [],
-                        'excerpt' => '',
-                        'attachment' => '',
-                        'sent' => 0
-                    ]);
-
-                    foreach($item['results'] as $network => $link)  {
-                        echo '<div>' . $network . ': ' . $link . '</div>';
-                    }
-
-                    printf('<div>%s</div>', $item['excerpt']);
-?>
-
-                <?php else: ?>
                 <?php
                     $item = wp_parse_args($item, [
                         'network' => [],
+                        'results' => [],
                         'excerpt' => '',
                         'attachment' => '',
-                        'delay' => 0
-                    ]);
-
-                    printf(
-                        '<input class="item__uniqid" data-item="uniqid" type="hidden" value="%s">',
-                        sanitize_title($uniqid)
-                    );
-
-                    $scheduled = wp_next_scheduled('knife_schedule_distribution', [
-                        sanitize_title($uniqid), absint($post_id)
+                        'delay' => 0,
+                        'sent' => 0
                     ]);
                 ?>
 
-                <div class="item__network">
-                    <?php
-                        foreach($channels as $channel => $label) {
-                            printf('<label class="item__network-check">%s<span>%s</span></label>',
-                                sprintf('<input type="checkbox" data-item="network" value="%s"%s>',
-                                    esc_attr($channel),
-                                    checked(in_array($channel, $item['network']), true, false)
-                                ),
+                <?php if(!empty($item['results'])) : ?>
 
-                                esc_html($label)
-                            );
-                        }
-                    ?>
-                </div>
-
-                <div class="item__snippet">
-                    <?php
-                        printf(
-                            '<textarea class="item__snippet-excerpt" data-item="excerpt" placeholder="%s">%s</textarea>',
-                            __('Напишите подвдоку для соцсетей', 'knife-theme'),
-                            sanitize_textarea_field($item['excerpt'])
-                        );
-                    ?>
-
-                    <figure class="item__snippet-poster">
+                    <div class="item__results">
                         <?php
-                            if(!empty($item['attachment'])) {
-                                printf('<img src="%s" alt="">',
-                                    wp_get_attachment_image_url($item['attachment'])
+                            foreach($item['results'] as $network => $link)  {
+                                echo '<div>' . $channels[$network] . ': ' . $link . '</div>';
+                            }
+
+                            printf('<div>%s</div>', $item['excerpt']);
+                        ?>
+                    </div>
+
+                <?php else : ?>
+
+                    <div class="item__network">
+                        <?php
+                            foreach($channels as $channel => $label) {
+                                printf(
+                                    '<label class="item__network-check">%s<span>%s</span></label>',
+
+                                    sprintf('<input type="checkbox" data-item="network" value="%s"%s>',
+                                        esc_attr($channel),
+                                        checked(in_array($channel, $item['network']), true, false)
+                                    ),
+                                    esc_html($label)
                                 );
                             }
                         ?>
-
-                        <?php
-                            printf(
-                                '<input class="item__snippet-attachment" data-item="attachment" type="hidden" value="%s">',
-                                sanitize_text_field($item['attachment'])
-                            );
-                        ?>
-
-                        <figcaption class="item__snippet-caption">+</figcaption>
-                        <span class="item__snippet-delete dashicons dashicons-trash"></span>
-                    </figure>
-                </div>
-
-                <?php if(is_numeric($scheduled)) : ?>
-                    <div class="item__schedule">
-                        <span class="dashicons dashicons-calendar"></span>
-
-                        <?php
-                            $scheduled = date('Y-m-d H:i:s', $scheduled);
-
-                            printf(
-                                '<span>%s</span><strong>%s</strong>',
-                                __('Задача запланирована на:', 'knife-theme'),
-                                get_date_from_gmt($scheduled, 'd.m.Y G:i')
-                            );
-
-                            printf(
-                                '<button class="item__schedule-cancel button-link" type="button">%s</button>',
-                                __('Отменить', 'knife-theme')
-                            );
-                        ?>
-
-                        <span class="spinner"></span>
                     </div>
-                <?php endif; ?>
 
-                <div class="item__delay">
                     <?php
-                        $timing = [
-                            '0' => __('Не отправлять автоматически', 'knife-theme'),
-                            '1' => __('Отправить в соцсети cразу', 'knife-theme'),
-                            '5' => __('Через 5 минут', 'knife-theme'),
-                            '15' => __('Через 15 минут','knife-theme'),
-                            '30' => __('Через 30 минут','knife-theme'),
-                            '60' => __('Через час','knife-theme')
-                        ];
-
-                        $options = [];
-
-                        foreach($timing as $delay => $label) {
-                            $options[] = sprintf('<option value="%1$s"%3$s>%2$s</option>',
-                                absint($delay), esc_html($label),
-                                selected($item['delay'], $delay, false)
-                            );
-                        }
-
-                        printf('<select class="item__delay-select" data-item="delay">%s</select>',
-                            implode('', $options)
+                        printf(
+                            '<input class="item__uniqid" data-item="uniqid" type="hidden" value="%s">',
+                            sanitize_title($uniqid)
                         );
 
-                        if(get_post_status(get_the_ID()) !== 'publish') {
-                            printf('<p class="item__delay-howto howto">%s</p>',
-                                __('после публикации поста', 'knife-theme')
-                            );
-                        }
+                        $scheduled = wp_next_scheduled('knife_schedule_distribution', [
+                            sanitize_title($uniqid), absint($post_id)
+                        ]);
                     ?>
-                </div>
 
-                <span class="item__delete dashicons dashicons-trash" title="<?php _e('Удалить задачу', 'knife-theme'); ?>"></span>
-                <?php endif;?>
+                    <div class="item__snippet">
+                        <?php
+                            printf(
+                                '<textarea class="item__snippet-excerpt" data-item="excerpt" placeholder="%s">%s</textarea>',
+                                __('Напишите подвдоку для соцсетей', 'knife-theme'),
+                                sanitize_textarea_field($item['excerpt'])
+                            );
+                        ?>
+
+                        <figure class="item__snippet-poster">
+                            <?php
+                                if(!empty($item['attachment'])) {
+                                    printf('<img src="%s" alt="">',
+                                        wp_get_attachment_image_url($item['attachment'])
+                                    );
+                                }
+                            ?>
+
+                            <?php
+                                printf(
+                                    '<input class="item__snippet-attachment" data-item="attachment" type="hidden" value="%s">',
+                                    sanitize_text_field($item['attachment'])
+                                );
+                            ?>
+
+                            <figcaption class="item__snippet-caption">+</figcaption>
+                            <span class="item__snippet-delete dashicons dashicons-trash"></span>
+                        </figure>
+                    </div>
+
+                    <?php if(is_numeric($scheduled)) : ?>
+                        <div class="item__schedule">
+                            <span class="dashicons dashicons-calendar"></span>
+
+                            <?php
+                                $scheduled = date('Y-m-d H:i:s', $scheduled);
+
+                                printf(
+                                    '<span>%s</span><strong>%s</strong>',
+                                    __('Задача запланирована на:', 'knife-theme'),
+                                    get_date_from_gmt($scheduled, 'd.m.Y G:i')
+                                );
+
+                                printf(
+                                    '<button class="item__schedule-cancel button-link" type="button">%s</button>',
+                                    __('Отменить', 'knife-theme')
+                                );
+                            ?>
+
+                            <span class="spinner"></span>
+                        </div>
+                    <?php endif; ?>
+
+                    <div class="item__delay">
+                        <?php
+                            $timing = [
+                                '0' => __('Не отправлять автоматически', 'knife-theme'),
+                                '1' => __('Отправить в соцсети cразу', 'knife-theme'),
+                                '5' => __('Через 5 минут', 'knife-theme'),
+                                '15' => __('Через 15 минут','knife-theme'),
+                                '30' => __('Через 30 минут','knife-theme'),
+                                '60' => __('Через час','knife-theme')
+                            ];
+
+                            $options = [];
+
+                            foreach($timing as $delay => $label) {
+                                $options[] = sprintf('<option value="%1$s"%3$s>%2$s</option>',
+                                    absint($delay), esc_html($label),
+                                    selected($item['delay'], $delay, false)
+                                );
+                            }
+
+                            printf('<select class="item__delay-select" data-item="delay">%s</select>',
+                                implode('', $options)
+                            );
+
+                            if(get_post_status(get_the_ID()) !== 'publish') {
+                                printf('<p class="item__delay-howto howto">%s</p>',
+                                    __('после публикации поста', 'knife-theme')
+                                );
+                            }
+                        ?>
+                    </div>
+
+                    <span class="item__delete dashicons dashicons-trash" title="<?php _e('Удалить задачу', 'knife-theme'); ?>"></span>
+
+                <?php endif; ?>
+
             </div>
         <?php endforeach; ?>
 
