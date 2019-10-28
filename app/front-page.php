e230b8cbfee544e18ce127e13ac28d3f3077a57a
Anton Lukin <anton@lukin.me>
2019-10-28 13:32:22 +0300

    replace longreads link in front-page template

--- app/front-page.php
+++ app/front-page.php
@@ -2,7 +2,7 @@
 /**
  * Template for showing site front-page
  *
- * Difference between front-page and home.php in a link below
+ * Difference between front-page and home.php on the link below
  *
  * @link https://wordpress.stackexchange.com/a/110987
  *
@@ -28,7 +28,7 @@ get_header(); ?>
         <?php
             printf('<a class="button" href="%2$s">%1$s</a>',
                 __('Все статьи', 'knife-theme'),
-                esc_url(home_url('/longreads/'))
+                esc_url(home_url('/longreads/page/2'))
             );
         ?>
     </nav>
