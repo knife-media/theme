847e1bdddd8f57928f5333a239e66d68b56335b0
Anton Lukin <anton@lukin.me>
2019-11-19 12:44:17 +0300

    fix header-navbar closing bug / #137

--- src/scripts/header-navbar.js
+++ src/scripts/header-navbar.js
@@ -14,6 +14,40 @@
 
 
   /**
+   * Toggle all elements before header
+   */
+  function toggleHeader(display) {
+    // Get first header parent child
+    var sibling = header.parentElement.firstChild;
+
+    while(sibling && sibling !== header) {
+      if(sibling.nodeType == 1) {
+        sibling.style.display = display;
+      }
+
+      sibling = sibling.nextSibling;
+    }
+  }
+
+
+  /**
+   * Close event menu outside
+   */
+  function clickOutside(e) {
+    var source = e.target || e.srcElement;
+
+    if(source === document.body) {
+      e.stopPropagation();
+
+      // Remove events
+      document.body.removeEventListener('click', clickOutside);
+
+      return toggle.click();
+    }
+  }
+
+
+  /**
    * Toggle menu bar
    */
   toggle.addEventListener('click', function(e) {
@@ -31,51 +65,31 @@
     // Set navbar expand class
     header.querySelector('.navbar').classList.toggle('navbar--expand');
 
-    // Get first header parent child
-    var sibling = header.parentElement.firstChild;
-
     // Close navbar menu
     if(body.classList.contains('is-navbar')) {
       body.classList.remove('is-navbar');
       body.style.top = '';
 
-      while(sibling && sibling !== header) {
-        if(sibling.nodeType == 1) {
-          sibling.style.display = '';
-        }
+      // Remove useless onclick attribute
+      body.removeAttribute('onclick', '');
 
-        sibling = sibling.nextSibling;
-      }
+      window.scrollTo(0, scrollTop);
 
-      return window.scrollTo(0, scrollTop);
-    }
-
-    // Hide all elements before header
-    while(sibling && sibling !== header) {
-      if(sibling.nodeType == 1) {
-        sibling.style.display = 'none';
-      }
-
-      sibling = sibling.nextSibling;
+      return toggleHeader('');
     }
 
     scrollTop = window.scrollY;
 
     body.classList.add('is-navbar');
     body.style.top = -scrollTop + 'px';
-  });
 
+    // Set attribite for iOS Safari
+    body.setAttribute('onclick', '');
 
-  /**
-   * Close menu on outside click
-   */
-  document.body.addEventListener('touchend', function(e) {
-    var source = e.target || e.srcElement;
+    // Add events on click outside menu
+    body.addEventListener('click', clickOutside);
 
-    if(this.classList.contains('is-navbar') && this === source) {
-      e.stopPropagation();
-
-      return toggle.click();
-    }
+    // Hide all elements before header
+    return toggleHeader('none');
   });
 })();
