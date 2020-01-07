733ec45ced16d22841a3ea17479bdcfcdf9aede0
Anton Lukin <anton@lukin.me>
2020-01-07 14:14:42 +0300

    remove vtb promo buttom from header

--- app/header.php
+++ app/header.php
@@ -30,8 +30,6 @@
 <header class="header">
     <div class="header__inner">
         <div class="header__logo">
-            <a class="header__logo-promo" href="https://knf.md/podarkivtb-btn/" target="_blank">VTB</a>
-
             <a class="header__logo-link" href="<?php echo esc_url(home_url('/')); ?>">
                 <svg class="header__logo-image" fill="#000000" x="0" y="0" viewBox="0 0 111 31.8" xml:space="preserve">
                     <g>
