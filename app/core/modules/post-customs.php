c2926183e47429b6b0f709a6068693afa464a3ef
Anton Lukin <anton@lukin.me>
2020-04-22 12:10:36 +0400

    remove knife-constitution customs / #163

--- app/core/modules/post-customs.php
+++ app/core/modules/post-customs.php
@@ -27,11 +27,6 @@ class Knife_Post_Customs {
      * Init function instead of constructor
      */
     public static function load_module() {
-        // Process custom posts ajax requests
-        if(wp_doing_ajax()) {
-            add_action('init', [__CLASS__, 'process_ajax']);
-        }
-
         // Load custom post functions
         add_action('wp', [__CLASS__, 'load_functions']);
 
@@ -55,7 +50,7 @@ class Knife_Post_Customs {
         // Get post name
         $name = $object->post_name;
 
-        if(array_key_exists($name, KNIFE_CUSTOMS)) {
+        if(in_array($name, KNIFE_CUSTOMS)) {
             $functions = "/core/customs/{$name}/functions.php";
 
             // Let's add the file if exists
@@ -63,43 +58,6 @@ class Knife_Post_Customs {
                 // Require customs
                 require_once(get_template_directory() .  $functions);
             }
-        }
-    }
-
-
-    /**
-     * Process custom posts ajax requests
-     */
-    public static function process_ajax() {
-        $action = (isset($_REQUEST['action'])) ? $_REQUEST['action'] : '';
-
-        if(strpos($action, 'knife-customs-') !== 0) {
-            return;
-        }
-
-        $name = str_replace('knife-customs-', '', $action);
-
-        // Get custom post functions file
-        $ajax = "/core/customs/{$name}/ajax.php";
-
-        // Let's add the file if exists
-        if(file_exists(get_template_directory() . $ajax)) {
-            // Require ajax customs
-            add_action('wp_ajax_' . $action, function() use ($action, $ajax) {
-                if(!check_ajax_referer($action, 'nonce', false)) {
-                    wp_send_json_error(__('Ошибка безопасности. Попробуйте еще раз', 'knife-theme'));
-                }
-
-                require(get_template_directory() . $ajax);
-            });
-
-            add_action('wp_ajax_nopriv_' . $action, function() use ($action, $ajax) {
-                if(!check_ajax_referer($action, 'nonce', false)) {
-                    wp_send_json_error(__('Ошибка безопасности. Попробуйте еще раз', 'knife-theme'));
-                }
-
-                require(get_template_directory() . $ajax);
-            });
         }
     }
 }
