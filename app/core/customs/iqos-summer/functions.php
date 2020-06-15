4dde47b7bd72bffaf8174fec58ec490fbcb325ab
Anton Lukin <anton@lukin.me>
2020-06-15 16:59:35 +0300

    add iqos-summer customs

---
+++ app/core/customs/iqos-summer/functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Iqos-summer: functions
+ *
+ * @package knife-theme
+ * @since 1.13
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
+
+/**
+ * Add custom styles
+ */
+add_action('wp_enqueue_scripts', function() {
+    $name = 'iqos-summer';
+
+    // Get styles
+    $styles = "/core/customs/{$name}/styles.css";
+
+    // Set styles version
+    $version = wp_get_theme()->get('Version');
+
+    if(defined('WP_DEBUG') && true === WP_DEBUG) {
+        $version = date('U');
+    }
+
+    // Let's add the file
+    wp_enqueue_style('knife-theme-' . $name, get_template_directory_uri() . $styles, ['knife-theme'], $version);
+});
