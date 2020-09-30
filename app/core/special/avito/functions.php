1f37e08235d2a65e7047a1ee35993a928cb776c0
Anton Lukin <anton@lukin.me>
2020-09-30 15:54:46 +0300

    add special avito styles

---
+++ app/core/special/avito/functions.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * avito: special functions
+ *
+ * @package knife-theme
+ * @since 1.14
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
+
+
+/**
+ * Add styles
+ */
+add_action('wp_enqueue_scripts', function() {
+    $slug = basename(__DIR__);
+
+    // Get theme version
+    $version = wp_get_theme()->get('Version');
+
+    if(defined('WP_DEBUG') && true === WP_DEBUG) {
+        $version = date('U');
+    }
+
+    $styles = "/core/special/{$slug}/styles.css";
+
+    // Let's add the file if exists
+    if(file_exists(get_template_directory() . $styles)) {
+        wp_enqueue_style('knife-special-' . $slug, get_template_directory_uri() . $styles, ['knife-theme'], $version);
+    }
+});
+
+
+/**
+ * Add button to post content
+ */
+add_action('the_content', function($content) {
+    global $post;
+
+    $slug = basename(__DIR__);
+
+    if(!property_exists('Knife_Special_Projects', 'taxonomy')) {
+        return $output;
+    }
+
+    $taxonomy = Knife_Special_Projects::$taxonomy;
+
+    if(has_term($slug, $taxonomy, $post) && is_main_query()) {
+        $promo_link = sprintf(
+            '<figure class="figure figure--promo"><a class="button" href="%s">%s <strong>%s</strong></a>',
+            esc_url(get_term_link($slug, $taxonomy)),
+            _x('Специальный проект', 'special: avito', 'knife-theme'),
+            _x('Журнала «Нож» и Авито.Недвижимость', 'special: avito', 'knife-theme')
+        );
+
+        $content = $content . $promo_link;
+    }
+
+    return $content;
+});
