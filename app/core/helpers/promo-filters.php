dd42afc256d6aba85c83e7d11660928c5ea3c57c
Anton Lukin <anton@lukin.me>
2020-11-30 14:40:07 +0300

    remove mts-switcher promo filter

--- app/core/helpers/promo-filters.php
+++ app/core/helpers/promo-filters.php
@@ -10,21 +10,3 @@
  * @version 1.14
  */
 
-
-/**
- * Add custom advertisment script for mts-switcher promo
- */
-add_action('wp_enqueue_scripts', function() {
-    // Get theme version
-    $version = wp_get_theme()->get('Version');
-
-    if(defined('WP_DEBUG') && true === WP_DEBUG) {
-        $version = date('U');
-    }
-
-    // Add scripts
-    wp_enqueue_script('knife-mts', '/external/mts-switcher/scripts.min.js', [], $version, true);
-
-    // Add styles
-    wp_enqueue_style('knife-mts', '/external/mts-switcher/styles.min.css', [], $version);
-});
