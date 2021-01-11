64b08b9fcb156ffe70c359a52693f8d9bf06d83c
Anton Lukin <anton@lukin.me>
2021-01-11 20:20:51 +0300

    add social-planner snippets

--- app/core/helpers/plugin-snippets.php
+++ app/core/helpers/plugin-snippets.php
@@ -19,3 +19,40 @@
 add_filter('ppp_nonce_life', function() {
     return 60 * 60 * 24 * 7; // 7 days
 });
+
+
+/**
+ * Hide settings for Social Planner plugin
+ *
+ * @since 1.14
+ */
+add_filter('social_planner_hide_settings', '__return_true');
+
+
+/**
+ * Add promo hashtag to Social Planner message
+ *
+ * @since 1.14
+ */
+add_filter('social_poster_prepare_message', function($message, $post_id) {
+    if(!class_exists('Knife_Promo_Manager')) {
+        return $message;
+    }
+
+    $meta_promo = Knife_Promo_Manager::$meta_promo;
+
+    if(get_post_meta($post_id, $meta_promo, true)) {
+        $excerpt = [];
+
+        if (!empty($message['excerpt'])) {
+            $excerpt[] = $message['excerpt'];
+        }
+
+        $excerpt[] = __('#партнерскийматериал', 'knife-theme');
+
+        // Append hashtag to message
+        $message['excerpt'] = implode("\n\n", $excerpt);
+    }
+
+     return $message;
+}, 10, 2);
