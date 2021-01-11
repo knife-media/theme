837d1ae20a13fff2627b125ba7af931e86efdf36
Anton Lukin <anton@lukin.me>
2021-01-12 02:32:23 +0300

    replace social-planner excerpt filter

--- app/core/helpers/plugin-snippets.php
+++ app/core/helpers/plugin-snippets.php
@@ -50,25 +50,22 @@ add_filter('social_planner_post_types', function($post_types) {
  *
  * @since 1.14
  */
-add_filter('social_poster_prepare_message', function($message, $post_id) {
+add_filter('social_planner_prepare_excerpt', function($excerpt, $message) {
+    $post_id = $message['post_id'];
+
     if(!class_exists('Knife_Promo_Manager')) {
-        return $message;
+        return $excerpt;
     }
 
     $meta_promo = Knife_Promo_Manager::$meta_promo;
 
     if(get_post_meta($post_id, $meta_promo, true)) {
-        $excerpt = [];
+        $hashtag =  __('#партнерскийматериал', 'knife-theme');
 
-        if (!empty($message['excerpt'])) {
-            $excerpt[] = $message['excerpt'];
+        if (!empty($excerpt)) {
+            $excerpt = $excerpt . "\n\n" . $hashtag;
         }
-
-        $excerpt[] = __('#партнерскийматериал', 'knife-theme');
-
-        // Append hashtag to message
-        $message['excerpt'] = implode("\n\n", $excerpt);
     }
 
-     return $message;
+    return $excerpt;
 }, 10, 2);
