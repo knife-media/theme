2c94bb5d113853f72f086a347dc7a28df0783d0b
Anton Lukin <anton@lukin.me>
2022-04-01 19:15:01 +0300

    upgrade social-planner snippets

--- app/core/helpers/plugin-snippets.php
+++ app/core/helpers/plugin-snippets.php
@@ -6,7 +6,7 @@
  *
  * @package knife-theme
  * @since 1.1
- * @version 1.14
+ * @version 1.15
  */
 
 
@@ -39,20 +39,85 @@ add_action('social_planner_task_sent', function($results, $key, $post_id) {
         return;
     }
 
+    $errors = [];
+
+    foreach ($results[$key]['errors'] as $key => $message) {
+        $title = $key;
+
+        if(class_exists('Social_Planner\Settings')) {
+            $providers = Social_Planner\Settings::get_providers();
+
+            $class = Social_Planner\Core::get_network_class($key);
+            $label = Social_Planner\Core::get_network_label($class);
+
+            if(!empty($providers[$key]['title'])) {
+                $label = $label . '. ' . $providers[$key]['title'];
+            }
+        }
+
+        $errors[] = "{$label}: {$message}";
+    }
+
     $timestamp = time();
 
     $data = [
         'nonce' => substr(sha1($secret . $timestamp), -12, 10),
         'time' => $timestamp,
-        'errors' => json_encode($results[$key]['errors']),
+        'errors' => json_encode($errors),
         'link' => get_permalink($post_id)
     ];
 
     wp_remote_post('https://knife.media/requests/planner', [
         'body' => http_build_query($data),
-        'method' => 'POST'
+        'blocking' => false,
     ]);
 }, 10, 3);
+
+
+/**
+ * Send notification if the post is not planned to Social Planner
+ */
+add_action('publish_post', function($post_id) {
+    if(!has_category('news', $post_id)) {
+        return;
+    }
+
+    if(!class_exists('Social_Planner\Metabox')) {
+        return;
+    }
+
+    $tasks = Social_Planner\Metabox::get_tasks($post_id);
+
+    if(!empty($tasks)) {
+        return;
+    }
+
+    wp_schedule_single_event(time() + 60, 'knife_schedule_requests_news', [$post_id]);
+}, 20);
+
+
+/**
+ * Social Planner forgotten news scheduler event
+ *
+ * @since 1.15
+ */
+add_action('knife_schedule_requests_news', function($post_id) {
+    $secret = empty(KNIFE_REQUESTS['secret']) ? '' : KNIFE_REQUESTS['secret'];
+
+    $timestamp = time();
+
+    $data = [
+        'nonce' => substr(sha1($secret . $timestamp), -12, 10),
+        'time' => $timestamp,
+        'link' => get_permalink($post_id)
+    ];
+
+    wp_remote_post('https://knife.media/requests/news', [
+        'body' => http_build_query($data),
+        'blocking' => false,
+    ]);
+});
+
 
 
 /**
